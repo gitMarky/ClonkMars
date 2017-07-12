692d839abb92a563f8147ed850b76a20d611fe9f
Mark <markh4b@hotmail.com>
2017-07-12 23:02:54 +0200

    Gravity
    Other scenarios could override this, so I did not want to call it
    Gravity_Mars

--- ClonkMars.ocd/Libraries.ocd/ScenarioMars.ocd/Script.c
+++ ClonkMars.ocd/Libraries.ocd/ScenarioMars.ocd/Script.c
@@ -3,4 +3,11 @@ func Initialize()
 {
 	_inherited(...);
     Sky_Mars();
+    Scenario_Gravity();
+}
+
+
+private func Scenario_Gravity()
+{
+    SetGravity(8); // 37% of default value 20, rounded up
 }
