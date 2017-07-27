b123c03c429b45356ed857a52b8d96e1bb2c13fd
Mark <markh4b@hotmail.com>
2017-07-27 21:20:23 +0200

    Construction site sign display

---
+++ ClonkMars.ocd/System.ocg/Mod_ConstructionSiteDisplay.c
@@ -0,0 +1,18 @@
+#appendto ConstructionSite
+
+
+public func Set(id type, int dir, object stick)
+{
+	_inherited(type, dir, stick);
+	
+	// dimensions
+	var w = type->~GetSiteWidth(direction, stick_to) ?? type->GetDefWidth();
+	//var h = type->~GetSiteHeight(direction, stick_to) ?? type->GetDefHeight();
+	
+	// invisible first layer
+	SetClrModulation(RGBa(255, 255, 255, 0));
+
+	// add construction site sign on top
+	SetGraphics(nil, ConstructionSiteEx, 3, GFXOV_MODE_ExtraGraphics);
+	SetObjDrawTransform(1000, 0, (-1 + dir * 2) * 500 * w + (1 - dir * 3) * 1000 * GetID()->GetDefWidth(), 0, 1000, 0, 3);
+}
