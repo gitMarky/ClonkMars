a055fe345e0d625344bf69e6b970679aa247782b
Mark <markh4b@hotmail.com>
2017-07-28 22:09:46 +0200

    Animations: Rolling with new OC function parameter

--- ClonkMars.ocd/System.ocg/Mod_Clonk_Rolling.c
+++ ClonkMars.ocd/System.ocg/Mod_Clonk_Rolling.c
@@ -1,31 +1,13 @@
 #appendto Clonk
 
 
-// Handle rolling when hitting the landscape
-public func HandleRollOnHit(int iXSpeed, int iYSpeed)
-{
-	if (this->IsWalking() && iYSpeed > 450)
-	{
-		// roll :D
-		var x_movement = ComDir2XY(GetComDir())[0];
-		var looking_right = GetDir() == DIR_Right;
-		if ((x_movement > 0 && looking_right) || (x_movement < 0 && !looking_right))
-		{
-			this->DoRoll(true); // the code is entirely the same, except that DoRoll is called with a parameter. Might add to the original code
-		}
-		else // Force kneel-down when hitting the ground at high velocity.
-			this->DoKneel(true);
-	}
-}
-
-
-private func DoRoll(bool forced)
+private func DoRoll(bool is_falling)
 {
 	// the suit slows you down
 	if (this->IsWearingSpaceSuit())
 	{
 		// kneel, if you are forced to roll by falling
-		if (forced)
+		if (is_falling)
 		{
 			this->DoKneel(true);
 		}
@@ -34,6 +16,6 @@ private func DoRoll(bool forced)
 	else
 	{
 		// roll normally without a suit
-		_inherited();
+		_inherited(is_falling);
 	}
 }
