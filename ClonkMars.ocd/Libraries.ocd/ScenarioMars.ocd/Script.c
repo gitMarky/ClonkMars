bcb853db14e788d2dd75df5b8aaf677078599c68
Mark <markh4b@hotmail.com>
2017-07-30 10:34:17 +0200

    Starting equipment

--- ClonkMars.ocd/Libraries.ocd/ScenarioMars.ocd/Script.c
+++ ClonkMars.ocd/Libraries.ocd/ScenarioMars.ocd/Script.c
@@ -11,6 +11,7 @@ public func Initialize()
 
 public func InitializePlayer(int player)
 {
+	CreateStartingMaterial(player);
 	CreateStartingEquipment(player);
 	CreateStartingCapsule(player);
 	GivePlayerBasicKnowledge(player);
@@ -29,14 +30,9 @@ private func CreateStartingCapsule(int player)
 {
 	var capsule = CreateSupplyCapsule(player);
 	capsule->SetLandingDestination();
-	for(var material in FindObjects(Find_Owner(player), Find_Or(Find_Category(C4D_Object), Find_OCF(OCF_CrewMember))))
+	for (var material in FindObjects(Find_Owner(player), Find_NoContainer(), Find_Or(Find_Category(C4D_Object), Find_OCF(OCF_CrewMember))))
 	{
 		material->Enter(capsule);
-	}
-	for (var crew in FindObjects(Find_Owner(player), Find_OCF(OCF_CrewMember)))
-	{
-		var suit = crew->CreateContents(SpaceSuit);
-		suit->PutOn(crew);
 	}
 }
 
@@ -47,7 +43,24 @@ private func CreateSupplyCapsule(int player)
 }
 
 
-private func CreateStartingEquipment(int player)
+private func CreateStartingMaterial(int player)
 {
 	CreateObjects({ConKit = 2, Metal = 2, Plastic = 2}, nil, nil, player);
+}
+
+private func CreateStartingEquipment(int player)
+{
+	var crew_members = FindObjects(Find_Owner(player), Find_OCF(OCF_CrewMember));
+	for (var i = 0; i < GetLength(crew_members); ++i)
+	{
+		var crew = crew_members[i];
+		CreateStartingEquipmentForCrewMember(player, crew, i);
+	}
+}
+
+private func CreateStartingEquipmentForCrewMember(int player, object crew, int crew_index)
+{
+	var suit = crew->CreateContents(SpaceSuit);
+	suit->PutOn(crew);
+	crew->CreateContents(Shovel);
 }
