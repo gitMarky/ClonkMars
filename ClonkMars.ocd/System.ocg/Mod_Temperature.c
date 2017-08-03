84a3e7ef2a6463d66f9c6f5f807957f0bb83aad8
Mark <markh4b@hotmail.com>
2017-08-03 19:53:31 +0200

    Temperature: Material specific

---
+++ ClonkMars.ocd/System.ocg/Mod_Temperature.c
@@ -0,0 +1,34 @@
+#appendto Temperature
+
+
+public func GetMaterialTemperature(int material)
+{
+	if (material == Material("DuroLava"))
+	{
+		return 500;
+	}
+	else if (material == Material("Lava"))
+	{
+	 	return 250;
+	}
+	else if (material == Material("Snow") || material == Material("Ice"))
+	{
+		return Min(GetTemperature(), 0);
+	}
+	else
+	{
+		var temperature = GetTemperature();
+
+		var is_solid = GetMaterialVal("Density", "Material", material) > 30;
+		var is_soil = GetMaterialVal("Soil", "Material", material) > 0;
+		var is_diggable = GetMaterialVal("DigFree", "Material", material) > 0;
+
+		var percent = 50;
+
+		if (is_soil) percent += 10;
+		if (is_diggable) percent += 10;
+		if (!is_solid) percent += 40;
+
+		return percent * temperature / 100;
+	}
+}
