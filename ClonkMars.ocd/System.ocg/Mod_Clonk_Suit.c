f3640a87ad3cfb7de0b6c305fba3a6a0322cf607
Mark <markh4b@hotmail.com>
2017-08-15 20:29:01 +0200

    Space Suit: Displays clonk head now.

--- ClonkMars.ocd/System.ocg/Mod_Clonk_Suit.c
+++ ClonkMars.ocd/System.ocg/Mod_Clonk_Suit.c
@@ -1,5 +1,6 @@
 #appendto Clonk
 
+local skin_mesh_helper;
 
 public func IsWearingSpaceSuit()
 {
@@ -19,39 +20,48 @@ public func SetSkin(int new_skin)
 	// Remember skin
 	skin = new_skin;
 	
-	//Adventurer
+	// Adventurer
 	if(skin == 0)
 	{
 		skin_name = "Clonk";
 		gender = 0;
 	}
 
-	//Steampunk
+	// Steampunk
 	if(skin == 1)
 	{
 		skin_name = "Steampunk";
 		gender = 1;
 	}
 
-	//Alchemist
+	// Alchemist
 	if(skin == 2)
 	{
 		skin_name = "Alchemist";
 		gender = 0;
 	}
 	
-	//Farmer
+	// Farmer
 	if(skin == 3)
 	{
 		skin_name = "Farmer";
 		gender = 1;
 	}
 
-	RemoveBackpack(); //add a backpack
+	// Does not need a backpack
+	RemoveBackpack();
 	
+	// Give a space suit (without helmet)
 	SetGraphics("Suit", SpaceSuit);
+	
+	// Attach a head that is matching the clonk skin
+	skin_mesh_helper = skin_mesh_helper ?? CreateObject(ClonkCustomSkins);
+	skin_mesh_helper->SetGraphics(Format("Head%s", skin_name));
+	skin_mesh_helper.Visibility = VIS_None;
+	AttachMesh(skin_mesh_helper, "skeleton_head", "skeleton_head", nil, AM_MatchSkeleton);
 
-	//refreshes animation (whatever that means?)
+	// Refreshes animation after changing the base graphics,
+	// because the clonk would be in a scarecrow position otherwise,
 	// Go back to original action afterwards and hope
 	// that noone calls SetSkin during more compex activities
 	var prev_action = GetAction();
@@ -59,4 +69,12 @@ public func SetSkin(int new_skin)
 	SetAction(prev_action);
 
 	return skin;
+}
+
+
+public func Destruction()
+{
+	if (skin_mesh_helper) skin_mesh_helper->RemoveObject();
+	
+	return _inherited(...);
 }
