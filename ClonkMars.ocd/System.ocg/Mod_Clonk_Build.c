56a49b22bd381a7bfd926b28531323ebb11b963d
Mark <markh4b@hotmail.com>
2017-08-27 11:56:53 +0200

    Construction: Better animation

--- ClonkMars.ocd/System.ocg/Mod_Clonk_Build.c
+++ ClonkMars.ocd/System.ocg/Mod_Clonk_Build.c
@@ -22,6 +22,10 @@ public func StartBuilding()
 public func StopBuilding()
 {
 	RemoveEffect("FxBuilding", this);
+	if (GetAction() == "Build")
+	{
+		SetAction("Walk");
+	}
 }
 
 
@@ -32,10 +36,18 @@ local FxBuilding = new Effect{
 		{
 			// Custom call, instead of DoCon
 			Target->GetActionTarget()->~DoConstructionProgress(1, Target);
+			// Action
+			Target->PlayAnimation("KneelDown", CLONK_ANIM_SLOT_Movement, Anim_Linear(0, 0, Target->GetAnimationLength("KneelDown"), this.KneelDuration, ANIM_Remove), Anim_Linear(0, 0, 1000, 30, ANIM_Remove));
+			// Particle effects
+			// TODO
+			// Sound effects
+			// TODO
 		}
 		else
 		{
 			Target->StopBuilding();
 		}
 	},
+	
+	KneelDuration = 30,
 };
