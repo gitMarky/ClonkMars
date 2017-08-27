ee7834abc2f645357091106ef290d1371c8b1a6c
Mark <markh4b@hotmail.com>
2017-08-27 12:31:22 +0200

    Panel: Behave like a structure

--- ClonkMars.ocd/Structures.ocd/Panel.ocd/Script.c
+++ ClonkMars.ocd/Structures.ocd/Panel.ocd/Script.c
@@ -1,3 +1,34 @@
+#include Library_Structure
+#include Library_DamageControl
+
+
+/* -- Production & research */
+
+public func IsMarsResearch() { return true; }
+public func IsBuildableByConKit() { return true; }
+public func GetBasementID(){ return Structure_Basement; }
+public func GetBasementWidth(){ return 40; }
+
+
+/* -- Properties -- */
+
+local Name = "$Name$";
+local Description = "$Description$";
+local ContainBlast = 1;
+local Components = { Metal=1, Plastic=1 };
+
+local ActMap = {
+construction_progress = {
+	Prototype = Action,
+	Procedure = DFA_NONE,
+	Name = "construction_progress",
+	Animation = "construction_progress",
+	Length = 1001,
+	Delay = 0,
+},
+};
+
+
 /* -- Engine callbacks -- */
 
 private func Construction()
@@ -10,10 +41,3 @@ private func Construction()
 	}
 }
 
-
-/* -- Properties -- */
-
-local Name = "$Name$";
-local Description = "$Description$";
-local ContainBlast = 1;
-local Components = { Metal=1, Plastic=1 };
