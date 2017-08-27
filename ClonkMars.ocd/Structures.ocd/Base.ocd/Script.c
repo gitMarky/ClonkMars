ab74d49371a250691307a77bfbfbca6a4d98ba8a
Mark <markh4b@hotmail.com>
2017-08-27 12:21:47 +0200

    Base: Updated picture

--- ClonkMars.ocd/Structures.ocd/Base.ocd/Script.c
+++ ClonkMars.ocd/Structures.ocd/Base.ocd/Script.c
@@ -22,6 +22,12 @@ local ContainBlast = true;
 local HitPoints = 50;
 local Components = { Metal = 2, Plastic = 2};
 
+private func Definition(id type)
+{
+	type.PictureTransformation = Trans_Mul(Trans_Scale(800, 800, 800), Trans_Translate(5000, -7500, +40000));
+}
+
+
 /* -- Door control -- */
 
 private func SoundOpenDoor()
