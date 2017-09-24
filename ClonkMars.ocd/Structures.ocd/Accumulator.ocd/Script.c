3ea9da5a172513665dd35b342820a1cf973abe92
Mark <markh4b@hotmail.com>
2017-09-24 22:28:59 +0200

    Accumulator: Storage functionality
    May change, however. Original CM allowed withdrawal of all power at
    once, should/could be changed to on-demand withdrawal

--- ClonkMars.ocd/Structures.ocd/Accumulator.ocd/Script.c
+++ ClonkMars.ocd/Structures.ocd/Accumulator.ocd/Script.c
@@ -1,6 +1,8 @@
 #include Library_MarsStructure
 #include Library_MarsResearch
 #include Library_ConstructionAnimation
+#include Library_PowerSystem_Storage
+#include Library_PowerSystem_DisplayStatus
 
 /* -- Properties -- */
 
@@ -8,3 +10,17 @@ local Name = "$Name$";
 local Description = "$Description$";
 local ContainBlast = 1;
 local Components = { Metal=2, Silicon=2 };
+local HitPoints = 25;
+
+/* -- Power System -- */
+
+
+private func Initialize()
+{
+	SetStoragePower(10);
+	SetStorageCapacity(1500 * POWER_SYSTEM_TICK); // 150 seconds * 10 production * 36 frames
+	SetProducerPriority(50);
+	RegisterPowerStorage(this);
+	
+	return _inherited(...);
+}
