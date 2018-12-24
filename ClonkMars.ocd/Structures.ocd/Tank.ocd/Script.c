e3bd59ebd89ab53c26b7f3f34e2bb97ecc52cca9
Mark <markh4b@hotmail.com>
2018-12-24 17:11:06 +0100

    Oiltank: Basics

--- ClonkMars.ocd/Structures.ocd/Tank.ocd/Script.c
+++ ClonkMars.ocd/Structures.ocd/Tank.ocd/Script.c
@@ -1,10 +1,20 @@
 #include Library_MarsStructure
 #include Library_MarsResearch
+#include Library_ConstructionAnimation
 
 
-/* -- Properties -- */
+/* --- Properties --- */
 
 local Name = "$Name$";
 local Description = "$Description$";
 local ContainBlast = 1;
 local Components = { Metal=3 };
+
+
+/* --- Engine Callbacks --- */
+
+func Definition(type)
+{
+	type.PictureTransformation = Trans_Translate(0, 0, -20000);
+	return _inherited(type, ...);
+}
