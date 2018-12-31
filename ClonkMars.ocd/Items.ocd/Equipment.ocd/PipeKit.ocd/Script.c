5f0897fc7261dc623c32dd563a7f8a807d0317ea
Mark <markh4b@hotmail.com>
2018-12-31 18:52:41 +0100

    Pipe kit: Mesh graphics

--- ClonkMars.ocd/Items.ocd/Equipment.ocd/PipeKit.ocd/Script.c
+++ ClonkMars.ocd/Items.ocd/Equipment.ocd/PipeKit.ocd/Script.c
@@ -6,3 +6,33 @@ local Name = "$Name$";
 local Description = "$Description$";
 local Collectible = 1;
 local Components = {Metal = 1};
+
+
+/* --- Display --- */
+
+public func GetCarryBone() { return "main"; }
+
+public func GetCarryMode(object clonk, bool secondary)
+{
+	if (secondary)
+	{
+		return CARRY_None;
+	}
+	return CARRY_BothHands;
+}
+
+public func GetCarryTransform(object user)
+{
+	return Trans_Mul(Trans_Rotate(90, 0, 0, 1), Trans_Translate(0, -1000, 0), Trans_Scale(800));
+}
+
+public func GetCarryPhase()
+{
+	return 1000;
+}
+
+public func Definition(proplist type)
+{
+	type.PictureTransformation = Trans_Mul(Trans_Rotate(-20, 1, 0, 0), Trans_Rotate(135, 0, 1, 0), Trans_Translate(1500, -500, 0));
+	type.MeshTransformation = Trans_Scale(800);
+}
